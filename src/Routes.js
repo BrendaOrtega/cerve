fbfecb4b1893cd41f579e280d541d8bdbc818abb
BrendaOrtega <bremin11.20.93@gmail.com>
2019-01-06 21:42:00 -0600

    estructura

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import HomeContainer from './components/home/HomeContainer';
+
+const Routes = () => (
+    <Switch>
+        <Route exact path="/" component={HomeContainer} />
+
+   </Switch>
+);
+
+export default Routes;
---
+++ src/components/home/HomeContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+
+class HomeContainer extends Component {
+
+    render() {
+        return (
+            <div>
+                    hola
+            </div>
+        );
+    }
+}
+
+export default HomeContainer;
